5afb13a4ce5f37d59bc687541f33827e50eb3f20
rhughes99 <rhughes99@comcast.net>
2021-05-15 12:30:44 -0400

    Update TrainBoard_A.py
    Display response mods

--- TrainBoard_A.py
+++ TrainBoard_A.py
@@ -28,57 +28,63 @@ blueLED.on()
 greenLED.on()
 redLED.on()
 
+blueLED.off()
+print('Blue')
+time.sleep(2)
+blueLED.on()
+
+greenLED.off()
+print('    Green')
+time.sleep(2)
+greenLED.on()
+
+redLED.off()
+print('         Red')
+time.sleep(2)
+redLED.on()
+
 while True:
     try:
-        blueLED.off()
-        print('Blue')
-        time.sleep(1)
-        blueLED.on()
-        
-        greenLED.off()
-        print('    Green')
-        time.sleep(1)
-        greenLED.on()
-        
-        redLED.off()
-        print('        Red')
-        time.sleep(1)
-        redLED.on()
-        
-        # Active input = 0, non pressed
+        # Active input = 0, not pressed
         if in1.is_pressed:
-            print('IN 1 ON')
+#            print('IN 1 ON')
             relayCmd &= 0b11111110
             bus.write_byte(busAddress, relayCmd)
+            blueLED.off()
         else:
-#            print('IN 1 off')
+#            print('    IN 1 off')
             relayCmd |= 0b00000001
             bus.write_byte(busAddress, relayCmd)
+            blueLED.off()
         
         if in2.is_pressed:
-            print('IN 2 ON')
+#            print('IN 2 ON')
             relayCmd &= 0b11111101
             bus.write_byte(busAddress, relayCmd)
+            greenLED.off()
         else:
-#            print('IN 2 off')
+#            print('    IN 2 off')
             relayCmd |= 0b00000010
             bus.write_byte(busAddress, relayCmd)
+            greenLED.off()
 
         if in3.is_pressed:
-            print('IN 3 ON')
+#            print('IN 3 ON')
             relayCmd &= 0b11111011
             bus.write_byte(busAddress, relayCmd)
+            redLED.off()
         else:
-#            print('IN 3 off')
+            print('    IN 3 off')
             relayCmd |= 0b00000100
             bus.write_byte(busAddress, relayCmd)
+			redLED.on()
 
         if in4.is_pressed:
             print('IN 4 ON')
             relayCmd &= 0b11110111
             bus.write_byte(busAddress, relayCmd)
         else:
-#            print('IN 4 off')
+            print('    IN 4 off')
             relayCmd |= 0b00001000
             bus.write_byte(busAddress, relayCmd)
 
@@ -87,7 +93,7 @@ while True:
             relayCmd &= 0b11101111
             bus.write_byte(busAddress, relayCmd)
         else:
-#            print('IN 5 off')
+            print('    IN 5 off')
             relayCmd |= 0b00010000
             bus.write_byte(busAddress, relayCmd)
 
@@ -96,7 +102,7 @@ while True:
             relayCmd &= 0b11011111
             bus.write_byte(busAddress, relayCmd)
         else:
-#            print('IN 2 off')
+            print('    IN 6 off')
             relayCmd |= 0b00100000
             bus.write_byte(busAddress, relayCmd)
 
