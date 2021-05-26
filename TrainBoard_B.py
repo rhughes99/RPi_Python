d63b273d60ff699c60aab95fcb06ca7727442e92
rhughes99 <rhughes99@comcast.net>
2021-05-26 14:20:20 -0400

    Integreation

--- TrainBoard_B.py
+++ TrainBoard_B.py
@@ -62,23 +62,36 @@ else:
     relayCmd |= 0b00000100
     bus.write_byte(busAddress, relayCmd)
     redLED.on()
-    previousSwitch == 0
+    previousSwitch = 0
+
+relay1 = 0
 
 while True:
     try:
         # Active input = 0, not pressed
         # Only react to changes in switch state
         if UncoupleButton.is_pressed and previousUncouple == 0:
-            print('UNCOUPLE pressed')
-            relayCmd &= 0b11111110
+#            print('UNCOUPLE pressed')
+            if relay1 == 0:
+                relayCmd &= 0b11111110
+#                bus.write_byte(busAddress, relayCmd)
+                blueLED.off()
+                relay1 = 1
+                print('ON')
+            else:
+                relayCmd |= 0b00000001
+#                bus.write_byte(busAddress, relayCmd)
+                blueLED.on()
+                relay1 = 0
+                print('   OFF')
+
             bus.write_byte(busAddress, relayCmd)
-            blueLED.off()
             previousUncouple = 1
         elif not(UncoupleButton.is_pressed) and previousUncouple == 1:
 #            print('    UNCOUPLE released')
-            relayCmd |= 0b00000001
-            bus.write_byte(busAddress, relayCmd)
-            blueLED.on()
+#            relayCmd |= 0b00000001
+#            bus.write_byte(busAddress, relayCmd)
+#            blueLED.on()
             previousUncouple = 0
 
         if UnloadButton.is_pressed and previousUnload == 0:
@@ -92,7 +105,7 @@ while True:
             relayCmd |= 0b00000010
             bus.write_byte(busAddress, relayCmd)
             greenLED.on()
-            previousUnload == 0
+            previousUnload = 0
 
         if Switch.is_pressed and previousSwitch == 0:
             print('Switch Out')
@@ -105,7 +118,7 @@ while True:
             relayCmd |= 0b00000100
             bus.write_byte(busAddress, relayCmd)
             redLED.on()
-            previousSwitch == 0
+            previousSwitch = 0
 
 #        if in4.is_pressed:
 #            print('IN 4 ON')
@@ -135,7 +148,6 @@ while True:
 #            bus.write_byte(busAddress, relayCmd)
 
         time.sleep(0.25)
-        print('')
 
     except KeyboardInterrupt as ki:
         bus.write_byte(busAddress, 0xFF)
