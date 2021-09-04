52f847ebec43540b6470197b73f38fa4cb398045
rhughes99 <rhughes99@comcast.net>
2021-09-04 11:20:06 -0400

    Update Usb_train_if.py

--- Usb_train_if.py
+++ Usb_train_if.py
@@ -1,114 +1,76 @@
-import os
-#os.environ['PYUSB_DEBUG'] = 'debug' # uncomment for verbose pyusb output
-import sys
-import platform
-from time import sleep
-
 import usb.core
-import usb.backend.libusb1
 import usb.util
+import sys
+#import usb.backend.libusb1
 
 VENDOR_ID =  0x0ABF
 PRODUCT_ID = 0x03ED
 
-def write_to_device(dev, msg_str):
-    print("Writing command: {}".format(msg_str))
-    # message structure:
-    #   message is an ASCII string containing the command
-    #   8 bytes in lenth
-    #   0th byte must always be 0x01
-    #   bytes 1 to 7 are ASCII character values representing the command
-    #   remainder of message is padded to character code 0 (null)
-    byte_str = chr(0x01) + msg_str + chr(0) * max(7 - len(msg_str), 0)
+# from Programming with PyUSB 1.0
+# https://github.com/pyusb/pyusb/blob/master/docs/tutorial.rst#id9
 
-    num_bytes_written = 0
+# find our device
+dev = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
 
-    try:
-        num_bytes_written = dev.write(0x01, byte_str)
-    except usb.core.USBError as e:
-        print (e.args)
+# was it found?
+if dev is None:
+    raise ValueError('*** Device not found')
 
-    return num_bytes_written
+print('Device found')
 
-def read_from_device(dev, timeout):
-    try:
-        data = dev.read(0x81, 64, timeout)
-    except usb.core.USBError as e:
-        print ("Error reading response: {}".format(e.args))
-        return None
+# set active configuration. With no arguments, first configuration will be active one
+dev.set_configuration()
 
-    byte_str = ''.join(chr(n) for n in data[1:])	# construct a string from read values, 
-    												# starting from 2nd byte
-    result_str = byte_str.split('\x00',1)[0]		# remove trailing null '\x00' chars
+# get an endpoint instance
+cfg = dev.get_active_configuration()
+intf = cfg[(0,0)]
 
-    if len(result_str) == 0:
-        return None
+# get endpoint instance
+ep = usb.util.find_descriptor(
+    intf,
+    # match the first OUT endpoint
+    custom_match = \
+    lambda e: \
+        usb.util.endpoint_direction(e.bEndpointAddress) == \
+        usb.util.ENDPOINT_OUT)
 
-    return result_str
+assert ep is not None
 
 
-was_kernel_driver_active = False
-device = None
+# iterate over a configuration to access interfaces, 
+#  and iterate over interfaces to access endpoints
+print('Iterating over configurations and interfaces...')
+for cfg in dev:
+    sys.stdout.write(str(cfg.bConfigurationValue) + '\n')
+    for intf in cfg:
+        sys.stdout.write('\t' + \
+                         str(intf.bInterfaceNumber) + \
+                         ',' + \
+                         str(intf.bAlternateSetting) + \
+                         '\n')
+        for ep in intf:
+            sys.stdout.write('\t\t' + \
+                             str(ep.bEndpointAddress) + \
+                             '\n')
 
-device = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
 
-# if OS kernel already claimed device
-#if device.is_kernel_driver_active(0) is True:
-#    # tell kernel to detach
-#    device.detach_kernel_driver(0)
-#    was_kernel_driver_active = True
+print('Sending ctrl_transfer...')
+data = 0x00000000
+dev.ctrl_transfer(0xF0, 0x40, 0, 0, data)
 
-if device is None:
-    raise ValueError('Device not found. Please ensure it is connected.')
-    sys.exit(1)
-
-#device.reset()
-
-# Set active configuration. With no arguments, first configuration will be active one
-#device.set_configuration()
-
-# Claim interface 0
-#usb.util.claim_interface(device, 0)
+print('Sending write...')
+data = 0x00000000000FFFFF
+ep.write(data)
 
 
 
 # bmRequestType =	USBmakebmRequestType(kUSBOut, kUSBVendor, kUSBDevice);
-reqType = 0xF0
-bReq = 0xF0			# SetIoPortsConfig
-wVal = 0			# unused
-wIndex = 0			# unused
+#reqType = 0xF0
+#bReq = 0xF0			# SetIoPortsConfig
+#wVal = 0			# unused
+#wIndex = 0			# unused
 # wLength	 = 4;						// 32bit int
 # pData    = &portBits;
 
 #device.ctrl_transfer(reqType, bReq, wVal, wIndex, data)
-device.ctrl_transfer(reqType, bReq, wVal, wIndex, 0x0FFF)
 
-
-
-# Write commands
-# bytes_written = write_to_device(device, 'SK0')	# set relay 0
-# bytes_written = write_to_device(device, 'RK0')	# reset relay 0
-
-print("Write #1")
-bytes_written = write_to_device(device, 'SK0')
-sleep(2)
-
-print("Write #2")
-bytes_written = write_to_device(device, 'SK0')
-sleep(2)
-
-
-# Read
-bytes_written = write_to_device(device, 'RPA')	# request value of PORT A in binary 
-data = read_from_device(device, 200)			# read from device 
-												# with a 200 millisecond timeout
-
-if data != None:
-    print("Received string: {}".format(data))
-    print("Received data as int: {}".format(int(data)))
-
-usb.util.release_interface(device, 0)
-
-# Reattach kernel driver if we previously detached it
-if was_kernel_driver_active == True:
-    device.attach_kernel_driver(0)
